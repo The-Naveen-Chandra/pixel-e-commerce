3809e533e86aaf560de5b4ce427310f326a68f31
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2023-12-03 23:23:59 +0530

    Update Header styles & button

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -1,19 +1,59 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 import { Header } from '../../../../payload/payload-types'
+import { noHeaderFooterUrls } from '../../../constants'
 import { Gutter } from '../../Gutter'
+import { HeaderNav } from '../Nav'
 
 import classes from './index.module.scss'
 
 const HeaderComponent = ({ header }: { header: Header }) => {
+  const pathname = usePathname()
+
+  // changing the theme of the logo based on the user's system theme
+  const [isDarkTheme, setIsDarkTheme] = useState(false)
+
+  useEffect(() => {
+    const darkThemeQuery = window.matchMedia('(prefers-color-scheme: dark)')
+    setIsDarkTheme(darkThemeQuery.matches)
+
+    const themeChangeListener = (event: MediaQueryListEvent) => {
+      setIsDarkTheme(event.matches)
+    }
+
+    darkThemeQuery.addEventListener('change', themeChangeListener)
+
+    return () => {
+      darkThemeQuery.removeEventListener('change', themeChangeListener)
+    }
+  }, [])
+
   return (
-    <nav className={[classes.header]}>
-      <Gutter className={[classes.wrap]}>
+    <nav
+      className={[
+        classes.header,
+        isDarkTheme ? classes.darkTheme : '',
+        noHeaderFooterUrls.includes(pathname) && classes.height,
+      ]
+        .filter(Boolean)
+        .join(' ')}
+    >
+      <Gutter className={classes.wrap}>
         <Link href={'/'}>
-          <Image src="/logo-black.svg" alt="logo" width={170} height={50} />
+          <Image
+            src={isDarkTheme ? '/logo-white.svg' : '/logo-black.svg'}
+            alt="logo"
+            width={110}
+            height={50}
+          />
         </Link>
+
+        <HeaderNav header={header} />
       </Gutter>
     </nav>
   )
